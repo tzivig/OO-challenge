dcc23ba7274ea4d9dd9e9435a9c20e4421506f1f
Tzivi <tzivigorowitz@gmail.com>
2023-01-19 10:57:00 -0500

    add code

--- OO-challenge.js
+++ OO-challenge.js
@@ -1,13 +1,50 @@
 class Vehicle {
-    constructor(make, model, year) {
-      this.make = make;
-      this.model = model;
-      this.year = year;
-    }
-    honk() {
-      return "Beep.";
-    }
-    toString(){
-        return `The Vehicle is a ${this.make} ${this.model} from ${this.year}`
-    }
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
   }
+  honk() {
+    return "Beep.";
+  }
+  toString(){
+      return `The Vehicle is a ${this.make} ${this.model} from ${this.year}`
+  }
+}
+
+
+class Car extends Vehicle {
+constructor(make,model,year){
+  super(make, model, year);
+  this.numWheels = 4;
+}
+}
+class Motorcycle extends Vehicle {
+constructor(make,model,year){
+  super(make, model, year);
+  this.numWheels = 2;
+}
+revEngine(){
+  return "VROOM!!!"
+}
+}
+
+class Garage{
+constructor(capacity){
+  this.vehicles = [];
+  this.capacity = capacity;
+}
+
+add(vehicle){
+  if (!(newVehicle instanceof Vehicle)) {
+    return "Only vehicles are allowed in here!";
+  }
+  if (this.vehicles.length >= this.capacity) {
+    return "Sorry, we're full.";
+  }
+  this.vehicles.push(newVehicle);
+  return "Vehicle added!";
+}
+}
+
+
